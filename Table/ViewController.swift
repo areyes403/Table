89f724fb7062a671095e3cd4b530af034a3a8a1c
areyes403 <areyes403>
2022-03-14 09:58:38 -0600

    Se agrego la tabla con algunas funcionalidades

--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableItems: UITableView!
+    var nombre:String?
+    
+    let alumnos = ["Fernando", "Jose", "Sahid"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableItems.delegate = self
+        tableItems.dataSource = self
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return alumnos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celda = tableItems.dequeueReusableCell(withIdentifier: "celda", for: indexPath)
+        celda.textLabel?.text = alumnos[indexPath.row]
+        celda.detailTextLabel?.text = "Esto es una prueba"
+        return celda
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(alumnos[indexPath.row])
+        nombre=alumnos[indexPath.row]
+        //Enviar datos a segunda view
+        performSegue(withIdentifier: "enviarNombre", sender: self )
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "enviarNombre"{
+            let objDestino=segue.destination as! detailedViewController
+            objDestino.recibirNombre = nombre
+            
+        }
+        
+    }
 }
 
---
+++ Table/detailedViewController.swift
@@ -0,0 +1,32 @@
+//
+//  detailedViewController.swift
+//  Table
+//
+//  Created by mac17 on 14/03/22.
+//
+
+import UIKit
+
+class detailedViewController: UIViewController {
+
+    @IBOutlet weak var lblNombre: UILabel!
+    var recibirNombre: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblNombre.text=recibirNombre
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
